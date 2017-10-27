d90013b07e456c0db07384529aad59453a41fb2e
Andre Cardim <ab.cardim@hotmail.com>
2017-10-27 08:56:11 +0000

    add somadigitos

--- aulas_pe_2017.3/25_10_17/somadigitos.c
+++ aulas_pe_2017.3/25_10_17/somadigitos.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 
-int somadigitos_TR( int n, )
+int somadigitos_TR( int n, int resp)
+{
+    if ( n==0 ) return resp;
+    return somadigitos_TR(n/10, resp+=n%10);
+}
 
 int somadigitos( int n )
 {
-    return somadigitos_TR( n, )
+    return somadigitos_TR( n, 0);
 }
-
 
 int main(){
     int n;
     
     scanf("%d", &n);
     
-    pritnf("%d", somadigitos(n));
+    printf("%d", somadigitos(n));
     
     return 0;
 }
 
 
-baseado na soma de digitos
+//baseado na soma de digitos
